d1f15e94810b1969407068d5b205ce2ba2ab1cba
Satyam Chauhan <its.satyam.592@gmail.com>
2026-01-06 18:25:48 +0530

    added live selfie capture

--- src/hooks/useCamera.jsx
+++ src/hooks/useCamera.jsx
@@ -2,49 +2,49 @@
 import { useRef, useState } from 'react'
 
 export function useCamera() {
-  const videoRef = useRef(null)
-  const canvasRef = useRef(null)
-  const [image, setImage] = useState(null)
-  const [isActive, setIsActive] = useState(false)
+    const videoRef = useRef(null)
+    const canvasRef = useRef(null)
+    const [image, setImage] = useState(null)
+    const [isActive, setIsActive] = useState(false)
+    //start camera    
+    const startCamera = async () => {
+        try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+            video: { facingMode: 'user' }
+        })
 
-  const startCamera = async () => {
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({
-        video: { facingMode: 'user' }
-      })
-
-      videoRef.current.srcObject = stream
-      setIsActive(true)
-    } catch (err) {
-      alert('Camera access denied')
+        videoRef.current.srcObject = stream
+        setIsActive(true)
+        } catch (err) {
+        alert('Camera access denied')
+        }
     }
-  }
 
-  const capturePhoto = () => {
-    const video = videoRef.current
-    const canvas = canvasRef.current
+    const capturePhoto = () => {
+        const video = videoRef.current
+        const canvas = canvasRef.current
 
-    canvas.width = video.videoWidth
-    canvas.height = video.videoHeight
+        canvas.width = video.videoWidth
+        canvas.height = video.videoHeight
 
-    canvas.getContext('2d').drawImage(video, 0, 0)
-    const imageData = canvas.toDataURL('image/png')
+        canvas.getContext('2d').drawImage(video, 0, 0)
+        const imageData = canvas.toDataURL('image/png')
 
-    setImage(imageData)
+        setImage(imageData)
 
-    // Stop camera
-    video.srcObject.getTracks().forEach(track => track.stop())
-    setIsActive(false)
+        // Stop camera
+        video.srcObject.getTracks().forEach(track => track.stop())
+        setIsActive(false)
 
-    return imageData
-  }
+        return imageData
+    }
 
-  return {
-    videoRef,
-    canvasRef,
-    image,
-    isActive,
-    startCamera,
-    capturePhoto
-  }
+    return {
+        videoRef,
+        canvasRef,
+        image,
+        isActive,
+        startCamera,
+        capturePhoto
+    }
 }
